69b252119bb070471d2894bc3d9e43758a02356e
cursivecode <cursivecode@gmail.com>
2014-05-01 22:30:42 -0400

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jasmine-cljs "0.1.4"
+(defproject jasmine-cljs "0.1.5"
   :description "Library for working with jasmine"
   :url "http://github.com/cursivecode/jasmine-cljs"
   :license {:name "Eclipse Public License"
